dac033356db5fbfe262972f7d2e34c31865b93f2
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-09-15 18:56:46 +0800

    update sesi-08

---
+++ Sesi-08/README.md
@@ -0,0 +1,133 @@
+# Sesi 8 - Styling Component, UI Frameworks, Styling App
+
+## Styling Component
+
+### 1. inline css dengan double curly braces dipisahkan dengan koma 
+    <Component style{{property:"value", property1:"value"}} />
+
+```jsx 
+function App() {
+    return (
+        <div style={{ backgroundColor: "#010101", padding: "30px" }}>
+            <h1>My First React App</h1>
+        </div>
+    );
+}
+```
+
+### 2. Object variable css
+
+```jsx
+const myStyle = {
+    backgroundColor: "#010101",
+    padding: "30px",
+};
+
+function App() {
+    return (
+        <div style={myStyle}> // variable
+            <h1>My First React App</h1>
+        </div>
+    );
+}
+```
+
+### 3. External CSS file
+
+```jsx
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>My First React App</h1>
+        </div>
+    );
+}
+```
+
+### 4. External CSS Module
+
+```jsx
+import styles from "./App.module.css"; // import css module
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <h1>My First React App</h1>
+        </div>
+    );
+}
+```
+
+### 5. External Object Variable
+    
+```jsx
+const myStyle = {
+    backgroundColor: "#010101",
+    padding: "30px",
+};
+```
+    
+```jsx
+import myStyle from "./App"; // import object variable
+
+function App() {
+    return (
+        <div style={myStyle}>
+            <h1>My First React App</h1>
+        </div>
+    );
+}
+```
+
+### 6. Styled Component
+
+```bash
+npm install styled-components
+```
+
+```jsx
+import styled from "styled-components";
+
+const MyDiv = styled.div`
+    background-color: #010101;
+    padding: 30px;
+`;
+
+function App() {
+    return (
+        <MyDiv>
+            <h1>My First React App</h1>
+        </MyDiv>
+    );
+}
+```
+### 7. CSS in JS library
+
+    contoh dengan library emotion
+```bash
+npm install @emotion/react @emotion/styled
+```
+
+```jsx
+import styled from "@emotion/styled";
+
+const MyDiv = styled.div`
+    background-color: #010101;
+    padding: 30px;
+`;
+
+function App() {
+    return (
+        <MyDiv>
+            <h1>My First React App</h1>
+        </MyDiv>
+    );
+}
+```
+
+## UI Frameworks
+
+### Contoh dengan Bootstrap
+[Open folder](./template/vision-ui-dashboard_MUI/)
---
+++ Sesi-08/UI-framework/index.js
@@ -0,0 +1,66 @@
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active text-decoration-underline" aria-current="page" href="#">Home</a>
+                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link" href="#">Pricing</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+function Card() {
+    return (
+        <div className="position-absolute top-50 start-50 translate-middle">
+        <div className="card p-5 bg-primary bg-opacity-10" style={{ width: "50rem" }}>
+            <div className="card-body">
+                <h5 className="card-title">First Paragraph</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Open Modal</button>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+function Modal() {
+    return (
+        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        This is a modal
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        {/* <button type="button" className="btn btn-primary">Save changes</button> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.Fragment>
+        <Navbar />
+        <Card />
+        <Modal />
+    </React.Fragment>
+)
