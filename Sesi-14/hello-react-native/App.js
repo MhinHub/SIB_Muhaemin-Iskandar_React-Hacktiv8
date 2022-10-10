2e136deff8f1a5db820882c6070d4a294d450c5b
MhinHub <iskandarm20d@student.unhas.ac.id>
2022-10-10 10:49:39 +0800

    Update Sesi-14

--- Sesi-14/hello-react-native/App.js
+++ Sesi-14/hello-react-native/App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo-768x528.png",
+        }}
+      />
+      <Text style={styles.text}>Mari Kita Belajar React Native</Text>
       <StatusBar style="auto" />
+      <Button title="Click Me" color="#31C6D4" />
     </View>
   );
 }
@@ -13,8 +20,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  image: {
+    height: 100,
+    width: 100,
   },
 });
